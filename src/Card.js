2810e07c0365010c69e7bb87420407ef677b3620
mr-exception <darbandi1996@gmail.com>
2017-08-10 22:31:29 +0430

    font awsome added

--- src/Card.js
+++ src/Card.js
@@ -4,58 +4,65 @@ const yellow = '#FFD712';
 class Card extends Component {
  constructor(props){
     super(props);
-    this.state = { color:"#ffff"};
-    this.changeColor = this.changeColor.bind(this);
   }
-  
-  changeColor(color) {
-    // const newColor = this.state.color == green ? yellow : green;
-    this.setState({ color: color});
+  state = {
+    color : "#FFF"
   }
-    render() {
-        return (
-            <div className="container">    
-              <div className="top-buffer row">
-                    <div className="col-md-8 col-sm-12 offset-md-2">
-                        <div className="card" style={{background: this.state.color}}>
-                            <div className="card-block">
-                                <div className="row">
-                                    <div className="col-lg-12">
-                                       <div className="input-group">
-                                        <input type="text" className="form-control" placeholder="add..." />
-                                        <span className="input-group-btn">
-                                        <button className="btn btn-secondary" type="button">Add!</button>
-                                        </span>
-                                        </div>
-                                        <select className="custom-select" onChange={(event) => {this.changeColor(event.target.color); }} >
-                                            <option selected>choose your favorite background Color</option>
-                                            <option color="#FFFF00" onClick={this.changeColor}>yellow</option>
-                                            <option color="#335BFF" onClick={this.changeColor}>blue</option>
-                                        </select>
-  <fieldset className="form-group top-buffer">
-   
-   <div className="form-check ">
-  <div className="form-check-label">
-    <input className="form-check-input" type="checkbox" id="inlineCheckbox1" value="option1"/> 1
-  </div>
-</div>
-<div className="form-check ">
-  <label className="form-check-label">
-    <input className="form-check-input" type="checkbox" id="inlineCheckbox2" value="option2"/> 2
-  </label>
-</div>
-    
-  </fieldset>
-                                    </div>
-                                </div>
-                             
-
-                            </div>
+  changeColor = (event) => {
+    this.setState({
+      color : event.target.value
+    });
+  }
+  render() {
+    return (
+      <div className="container">    
+        <div className="top-buffer row">
+          <div className="col-md-8 col-sm-12 offset-md-2">
+            <div className="card" style={{background: this.state.color}}>
+              <div className="card-block">
+                <div className="row">
+                  <div className="col-lg-12">
+                    <div className="input-group">
+                    <input type="text" className="form-control" placeholder="add..." />
+                      <span className="input-group-btn">
+                        <button className="btn btn-secondary" type="button">Add!</button>
+                      </span>
                     </div>
-                 </div> 
-                 </div>
+                    <fieldset className="form-group top-buffer">
+                    <div className="form-check ">
+                      <div className="form-check-label">
+                        <input className="form-check-input" type="checkbox" id="inlineCheckbox1" value="option1"/> 1
+                      </div>
+                    </div>
+                      <div className="form-check ">
+                        <label className="form-check-label">
+                          <input className="form-check-input" type="checkbox" id="inlineCheckbox2" value="option2"/> 2
+                        </label>
+                      </div>
+                    </fieldset>
+                  </div>
+                </div>
+                <div className="row">
+                  <div className="col-md-3 col-sm-12 pull-right">
+                    <select className="custom-select" onChange={this.changeColor} >
+                      <option value="#FFF" onClick={this.changeColor}>
+                        {this.state.color == "#FFF"? <i className="fa fa-check" aria-hidden="true"></i> : ''}white
+                      </option>
+                      <option value="#FFFF00" onClick={this.changeColor}>
+                        {this.state.color == "#FFFF00"? <i className="fa fa-check" aria-hidden="true"></i> : ''}yellow
+                      </option>
+                      <option value="#335BFF" onClick={this.changeColor}>
+                        {this.state.color == "#335BFF"? <i className="fa fa-check" aria-hidden="true"></i> : ''}blue
+                      </option>
+                    </select>
+                  </div>
+                </div>
+              </div>
             </div>
-        );
-    }
+          </div> 
+        </div>
+      </div>
+    );
+  }
 }
 export default Card;
